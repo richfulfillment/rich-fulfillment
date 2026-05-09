240f6fe8b37dce1f497ea98718f0661729b90cc3
richfulfillment <elizabethbremang@gmail.com>
2026-05-09 22:53:46 +0000

    Update Home component with service details

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    </div> 
-  <h/1>Rich Fulfillment Catering Services</h1>
+    <div>
+      <div>
+        <h1>Rich Fulfilment Catering Services</h1>
+      </div>
+
+      <div>
+        <h2>Types of Services</h2>
+        <p>Western Food, Local Food, African Food</p>
+      </div>
+
+      <div>
+        <p>Luxury Ghanaian-inspired catering experience</p>
+      </div>
     </div>
   );
 }
